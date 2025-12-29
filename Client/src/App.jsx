14e31e42a04109136b580f61b80146d71d0746cc
Emilio Zalva <emizalva08@gmail.com>
2025-12-29 11:58:17 -0300

    primera configuracion react - tailwind // frontend

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen text-white flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-orange-500">
+        Hola Liga de Básquet 🏀
+      </h1>
+    </div>
+  )
+}
+
+export default App
